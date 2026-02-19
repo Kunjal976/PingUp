215526475b6d81031c05750dfdb3693d363053ea
Kunjal976 <kunjalsaxena@gmail.com>
2026-02-19 23:52:20 +0530

    inngest error fix

--- server/server.js
+++ server/server.js
@@ -3,11 +3,12 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { inngest, functions } from './inngest/index.js'
+import{serve} from 'inngest/express'
 const app = express();
 
 await connectDB();
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Server is running'))
